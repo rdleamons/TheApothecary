6c9ca319059366e6d5048898259cda1143555c67
Aiden Thacker <aidenthacker86@gmail.com>
2023-02-03 23:22:48 -0600

    Character and Camera Movement

---
+++ Assets/Scripts/Aiden's Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    public Camera camera;
+
+    Vector2 movement;
+
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDic = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDic.y, lookDic.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
