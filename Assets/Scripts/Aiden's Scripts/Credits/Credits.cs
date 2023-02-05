5171c498271a3268b7062be0b569ba9d78cad671
Aiden Thacker <aidenthacker86@gmail.com>
2023-02-05 11:19:25 -0600

    Credits Scene Working

---
+++ Assets/Scripts/Aiden's Scripts/Credits/Credits.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    
+    public string sceneName;
+
+    public int secondsPast = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        secondsPast++;
+        Debug.Log(secondsPast);
+
+        if(secondsPast >= 1200)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
